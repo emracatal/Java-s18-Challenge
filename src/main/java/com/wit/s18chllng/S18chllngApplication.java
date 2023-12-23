e33d484da750e181fe10c6f413d1cd0a53934716
emracatal <134175723+emracatal@users.noreply.github.com>
2023-12-23 16:03:12 +0300

    initial commit

---
+++ src/main/java/com/wit/s18chllng/S18chllngApplication.java
@@ -0,0 +1,13 @@
+package com.wit.s18chllng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S18chllngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S18chllngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wit/s18chllng/S18chllngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wit.s18chllng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S18chllngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
